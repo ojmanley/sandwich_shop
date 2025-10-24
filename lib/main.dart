822e4c94e3c02402572fa26e4c24d5298e21e18d
Owen J Manley <owenjmanley@gmail.com>
2025-10-24 14:06:54 +0100

    define the UI inside app

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,17 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
-  }
+  return MaterialApp(
+    title: 'Sandwich Shop App',
+    home: Scaffold(
+      appBar: AppBar(title: const Text('Sandwich Counter')),
+      body: const Center(
+        child: Text('Welcome to the Sandwich Shop!'),
+      ),
+    ),
+  );
+}
+
 }
 
 class MyApp extends StatelessWidget {
